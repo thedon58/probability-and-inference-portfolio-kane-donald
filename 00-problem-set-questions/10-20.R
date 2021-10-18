6a1d12afce76288a91a82961acc62611211ce63b
thedon58 <donald23kane@gmail.com>
2021-10-18 09:45:18 -0500

    save workspace

---
+++ 00-problem-set-questions/10-20.R
@@ -0,0 +1,12 @@
+# (a) Generate, via simulation, a plot of the distribution of the 
+# 25th percentile of a sample of size 100 when the underlying 
+# distribution is gamma with shape = 3 and scale = 12 
+# (b) Overlay on the plot from (a) the analytic solution of the pdf
+
+require(tgsify)
+
+q_bmi <- array(NA, 5000)
+for(i in seq_along(q_bmi)) {
+  q_bmi[1] <- rgamma(100, shape = 3, scale = 12) %>% quantile(0.25)
+}
+hist(q_bmi, freq = FALSE, breaks = 1000, add=TRUE)
