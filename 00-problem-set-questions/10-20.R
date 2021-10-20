6a7a8d1bca0466caeca143b89d1fdf72dd03b291
thedon58 <donald23kane@gmail.com>
2021-10-20 09:13:19 -0500

    t

--- 00-problem-set-questions/10-20.R
+++ 00-problem-set-questions/10-20.R
@@ -3,10 +3,12 @@
 # distribution is gamma with shape = 3 and scale = 12 
 # (b) Overlay on the plot from (a) the analytic solution of the pdf
 
-require(tgsify)
-
-q_bmi <- array(NA, 5000)
-for(i in seq_along(q_bmi)) {
-  q_bmi[1] <- rgamma(100, shape = 3, scale = 12) %>% quantile(0.25)
+library(tidyverse)
+q_bmi <- array(NA, 500)
+v <- array(NA, 5000)
+for (i in seq_along(q_bmi)){
+  t <- rgamma(100, shape = 3, scale = 12)
+  v[i] <- quantile(t, .25)
 }
-hist(q_bmi, freq = FALSE, breaks = 1000, add=TRUE)
+hist(v, breaks = 100, freq = FALSE, main = "")
+curve(dgamma(x, 3, scale=12), lwd = 3, add = TRUE, col = "blue")
